b55351650a4177a295a592e24f267d84317fb2db
ohbeomho <ohbeomho0322@gmail.com>
2025-02-01 22:17:15 +0900

    🚧 Add reducer to manage tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from 'react'
+
+type Task = {
+  id: string
+  text: string
+}
+
+type TaskAction = {
+  type: 'add' | 'remove' | 'mark'
+  id?: string
+  text?: string
+  done?: boolean
+}
+
+function tasksReducer(tasks: Task[], action: TaskAction) {
+  let updated: Task[]
+
+  switch (action.type) {
+    case 'add':
+      updated = [...tasks, { id: crypto.randomUUID(), text: action.text! }]
+      break
+    case 'remove':
+      updated = tasks.filter((task) => task.id !== action.id!)
+      break
+    case 'mark':
+      updated = tasks.map((task) =>
+        task.id === action.id! ? { ...task, done: action.done! } : task,
+      )
+  }
+
+  localStorage.setItem('tasks', JSON.stringify(updated))
+  return updated
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, dispatch] = useReducer(tasksReducer, [])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <></>
 }
 
 export default App
