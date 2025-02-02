f769380cd6f4c75eb92ab97dc7c0bf22d9dda87d
ohbeomho <ohbeomho0322@gmail.com>
2025-02-02 21:38:26 +0900

    Design: Layout change

--- src/components/List.styled.tsx
+++ src/components/List.styled.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 const TaskList = styled.ul`
+  width: 100%;
   list-style: none;
   padding: 0;
   margin: 0;
@@ -10,26 +11,38 @@ const TaskListItem = styled.li.attrs<{ $done?: boolean }>((props) => ({
   $done: props.$done || false,
 }))`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  align-items: stretch;
   background-color: rgb(240, 240, 240);
   padding: 0.4rem;
+  width: calc(100% - 0.8rem);
 
   &:nth-child(even) {
     background-color: rgb(225, 225, 225);
   }
 
   &:first-child {
-    border-radius: 0.4rem 0.4rem 0px 0px;
+    border-top-left-radius: 0.4rem;
+    border-top-right-radius: 0.4rem;
   }
 
   &:last-child {
-    border-radius: 0px 0px 0.4rem 0.4rem;
+    border-bottom-left-radius: 0.4rem;
+    border-bottom-right-radius: 0.4rem;
   }
 
-  & label {
+  & > div:first-child {
+    padding: 0 0.2rem 0 0.2rem;
     text-decoration: ${(props) => (props.$done ? 'line-through' : 'none')};
     color: ${(props) => (props.$done ? 'gray' : 'inherit')};
+    line-break: anywhere;
+  }
+
+  & > div:last-child {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 `
 
